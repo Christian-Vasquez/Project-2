4d8763ad4429d616d414356ce50dc186cad024a1
rudcks91 <rudcks1991@gmail.com>
2017-12-13 18:48:47 -0500

    dates fixed

--- public/js/view.js
+++ public/js/view.js
@@ -3,19 +3,6 @@ $(document).ready(function() {
   var days = 0;
   var plandays = 1;
 
-// html in card
-  var card = '<div class="card">';
-  card += '<div class="card-header">';
-  card += 'Day' + plandays;
-  card += '</div>';
-  card += '<div class="card-block">';
-  card += '<blockquote class="card-blockquote">';
-  card += '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>';
-  card += '<footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>';
-  card += '</blockquote>';
-  card += '</div>';
-  card += '</div> <br>';
-
   $(".createPlannerButton").click(function(){
     console.log("createPlannerButton clikced")
     var start = $('#from').datepicker('getDate');
@@ -25,10 +12,21 @@ $(document).ready(function() {
     console.log(days);
 
     for(days; days > 0; days --){
+        var card = '<div class="card">';
+  card += '<div class="card-header">';
+  card += 'Day ' + plandays;
+  card += '</div>';
+  card += '<div class="card-block">';
+  card += '<blockquote class="card-blockquote">';
+  card += '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>';
+  card += '<footer>Someone famous in <cite title="Source Title">Source Title</cite></footer>';
+  card += '</blockquote>';
+  card += '</div>';
+  card += '</div> <br>';
 
-    // plandays++;
+
+    plandays++;
     $("#planner").append(card);
-    plandays = days+1;
   }
 
     checkup();
